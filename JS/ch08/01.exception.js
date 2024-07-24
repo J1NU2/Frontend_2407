36ba3ea0b168b12b57d8c8f1dff5e5e86c67c410
J1NU2 <sdrtst@naver.com>
2024-07-24 11:31:14 +0900

    7월24일 JS 오전 강의 내용

---
+++ JS/ch08/01.exception.js
@@ -0,0 +1,12 @@
+console.log('프로그램 시작');
+
+/*
+console.rog('오류 발생 지점'
+> SyntaxError: missing ) after argument list
+*/
+
+
+/*
+console.rog('오류 발생 지점');
+> TypeError: console.rog is not a function
+*/
